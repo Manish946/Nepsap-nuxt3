9a5510699292ef39c0bd48130e5061b82adf977f
Manish946 <manishestha@gmail.com>
2022-09-17 20:41:20 +0200

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
   }
   ,
-  ssr: true,
   meta: {
     title: 'NepSap',
     meta: [
@@ -31,41 +30,9 @@ export default defineNuxtConfig({
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/style.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    // https://go.nuxtjs.dev/stylelint
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-   
-
-  ],
-
-
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
-    },
-  },
-
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-  server: {
-    host: '0.0.0.0',
-  }
 
 })
