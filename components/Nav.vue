920b93ace7b15586af0d952d37c9a08003a1d193
Manish946 <manishestha@gmail.com>
2022-09-17 22:26:49 +0200

    test9

--- components/Nav.vue
+++ components/Nav.vue
@@ -102,9 +102,7 @@ export default({
           >
           <a class="signin-button" v-if="!user" href="#">Sign Up</a>
           <a @click="signOut" v-if="user" class="login-button">Logout</a>
-          <client-only v-if="user">
-          <p style="color:white">{{user.displayName}}</p>
-          </client-only>
+
       </div>
 
       <div>
