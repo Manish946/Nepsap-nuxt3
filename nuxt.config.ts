aed6c772af32571869a23d82f1d1e7bff0dcc52a
Manish946 <manishestha@gmail.com>
2022-09-18 15:55:35 +0200

    fix on local.-

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,13 +22,13 @@ export default defineNuxtConfig({
         rel: 'stylesheet',type:'text/css',
         href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
       },
-      { rel: 'icon', type: 'image/png', href: './public/PageLogo.png' },
+      { rel: 'icon', type: 'image/png', href: '/PageLogo.png' },
     ],
   },
   type: ['vuetify'],
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/public/assets/style.css'],
+  css: ['public/assets/style.css'],
 
 
   // Auto import components: https://go.nuxtjs.dev/config-components
