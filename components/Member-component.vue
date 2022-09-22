0b9da26227935b845844e5cf71189862adc9bf28
Manish946 <manishestha@gmail.com>
2022-09-22 23:43:10 +0200

    fix.

--- components/Member-component.vue
+++ components/Member-component.vue
@@ -1,9 +1,36 @@
 <template>
-
 <div class="member-header" v-if="members">
       <h1>Members</h1>
       <h3>Following is the list of the NepSAP members. We periodically update our members list.</h3>
     </div>
+
+    <v-card
+    class="mx-auto"
+  >
+<v-list density="compact">
+      <v-list-subheader >NEPSAP MEMBERS</v-list-subheader>
+   <v-list-item
+        v-for="(item, i) in members"
+        :key="i"
+        :value="item"
+        active-color="primary"
+        variant="tonal"
+        
+      >
+          <v-list-item-avatar size="40">
+            <v-avatar>
+            <v-img v-if="item.profile" v-bind:src=item.profile></v-img>
+            <v-img v-if="!item.profile" src="https://firebasestorage.googleapis.com/v0/b/nepsap-website.appspot.com/o/Members%2Fdefault.png?alt=media&token=6ae99e14-2486-4a05-8de5-71847f1de7ab"></v-img>
+
+            </v-avatar>
+          </v-list-item-avatar>
+
+        <v-list-item-title v-text="item.Name" ></v-list-item-title>
+        
+   </v-list-item>
+
+</v-list>
+  </v-card>
   <div class="members-wrapper">
     <div v-for="member in members" :key="member.uid" >
     <h1>Name : {{member.Name}}</h1>
@@ -57,6 +84,7 @@ export default {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  margin-top: 35px;
 }
 
 .members-wrapper{
