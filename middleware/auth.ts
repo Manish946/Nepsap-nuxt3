9cff989806469e031d958ec02a7ffe2e164041ce
Manish946 <manishestha@gmail.com>
2022-10-23 20:18:41 +0200

    Auth withoutlogin Admin page. if statement,

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,20 @@
 export default defineNuxtRouteMiddleware((to) => {
     const CookieUser = useCookie('CookieUser')
-    const user = JSON.parse(JSON.stringify(CookieUser.value))
-    const config = useRuntimeConfig();
-    if (user.uid == config.ADMIN_KEY) {
-      console.log('Accessed runtime config within middleware.')
+   
+    if(CookieUser.value){
+      const user = JSON.parse(JSON.stringify(CookieUser.value))
+      const config = useRuntimeConfig();
+      if (user.uid == config.ADMIN_KEY) {
+        console.log('Accessed runtime config within middleware.')
+      }
+      else{
+          console.log('Heading to', to.path, 'but I think we should go somewhere else...')
+          return '/middleware'
+      }
+      
+    }else{
+      return '/login'
     }
-    else{
-        console.log('Heading to', to.path, 'but I think we should go somewhere else...')
-        return '/middleware'
-    }
-    
+   
   })
   
